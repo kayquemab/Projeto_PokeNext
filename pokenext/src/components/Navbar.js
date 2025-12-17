c525579a940af57917a1786c89902239c715ccf5
Kayque Miqueias <kayquemiqueias17@gmail.com>
2025-12-17 13:43:39 -0300

    Ajustes

--- pokenext/src/components/Navbar.js
+++ pokenext/src/components/Navbar.js
@@ -10,6 +10,7 @@ export default function Navbar() {
     { label: "Times", href: "/Times" },
   ];
 
+  
   return (
     <header className="relative flex justify-center px-4 sm:px-6 lg:px-8 py-4 h-20">
       <motion.div
